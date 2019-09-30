5f4d1e25c87406223926164bafa998d61790ce2e
zbcn8 <zbcn810@163.com>
2019-09-30 17:14:35 +0800

    本地缓存重复提交

--- duplicate-request/src/main/java/com/zbcn/duplicaterequest/controller/DemonController.java
+++ duplicate-request/src/main/java/com/zbcn/duplicaterequest/controller/DemonController.java
@@ -2,6 +2,7 @@ package com.zbcn.duplicaterequest.controller;
 
 import com.zbcn.duplicaterequest.annotation.CacheLock;
 import com.zbcn.duplicaterequest.annotation.CacheParam;
+import com.zbcn.duplicaterequest.annotation.LocalLock;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,9 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/duplicate")
 public class DemonController {
 
+	/**
+	 * redis 重复提交测试
+	 * @param token
+	 * @return
+	 */
 	@CacheLock(prefix = "books")
 	@GetMapping
-	public String query(@CacheParam(name = "token") @RequestParam String token) {
+	public String redisQuery(@CacheParam(name = "token") @RequestParam String token) {
+		return "success - " + token;
+	}
+
+	/**
+	 * 基于 guava 重复提交测试
+	 * @param token
+	 * @return
+	 */
+	@LocalLock(key = "book:arg[0]")
+	@GetMapping("/local")
+	public String localQuery(@RequestParam String token) {
 		return "success - " + token;
 	}
 }
