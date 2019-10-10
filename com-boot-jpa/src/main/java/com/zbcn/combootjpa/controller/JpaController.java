ae9a1c120c87b949b5ac5467b48ad750a3ec5dc1
zbcn8 <zbcn810@163.com>
2019-10-10 09:11:50 +0800

    springboot -jpa

--- com-boot-jpa/src/main/java/com/zbcn/combootjpa/controller/JpaController.java
+++ com-boot-jpa/src/main/java/com/zbcn/combootjpa/controller/JpaController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @RestController
