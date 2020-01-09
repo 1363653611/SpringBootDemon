334f6511b60219351d99478abfa3a76fb69ac971
1363653611 <zbcn810@163.com>
2020-01-09 13:49:26 +0800

    springboot -项目整理

---
+++ com-boot-security/src/test/java/com/zbcn/combootsecurity/ComBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbcn.combootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
