3aaa10d5f8d45c1b95ccbd733b615435b9d3f05d
zbcn <zbcn810@163.com>
2021-01-06 19:16:52 +0800

    springboot: starter 学习

--- com-boot-starter-zbcn/src/main/java/com/zbcn/ZbcnServiceAutoConfiguration.java
+++ com-boot-starter-zbcn/src/main/java/com/zbcn/ZbcnServiceAutoConfiguration.java
@@ -11,13 +11,15 @@ package com.zbcn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableConfigurationProperties(ZbcnServiceProperties.class)//使@ConfigurationProperties注解生效
+@Configuration//定义为配置类
+@ConditionalOnWebApplication //在web工程条件下成立
+@EnableConfigurationProperties(ZbcnServiceProperties.class)//启用ZbcnServiceProperties配置功能，并加入到IOC容器中
 @ConditionalOnClass(ZbcnService.class)//当某个类存在的时候自动配置这个类
 public class ZbcnServiceAutoConfiguration {
 
--- com-boot-starter-zbcn/src/main/java/com/zbcn/ZbcnServiceProperties.java
+++ com-boot-starter-zbcn/src/main/java/com/zbcn/ZbcnServiceProperties.java
@@ -1,6 +1,7 @@
 package com.zbcn;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author zbcn8
@@ -10,6 +11,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @createTime 2019年09月07日 17:51:00
  */
 @ConfigurationProperties(prefix = "zbcn")
+//自动生成 spring-configuration-metadata 文件，代替默认
+@PropertySource(value = {"classpath:META-INF/spring-configuration-metadata.json"},
+        ignoreResourceNotFound = false, encoding = "UTF-8", name = "spring-configuration-metadata.json")
 public class ZbcnServiceProperties {
 
     private static final String MSG = "hello";
