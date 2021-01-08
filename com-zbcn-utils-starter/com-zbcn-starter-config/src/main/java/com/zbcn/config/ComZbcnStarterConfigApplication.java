751e0f56d713f570fd954b3b8a6c77a5883c33cf
zbcn <zbcn810@163.com>
2021-01-08 20:06:26 +0800

    springboot: starter 学习

---
+++ com-zbcn-utils-starter/com-zbcn-starter-config/src/main/java/com/zbcn/config/ComZbcnStarterConfigApplication.java
@@ -0,0 +1,13 @@
+package com.zbcn.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComZbcnStarterConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComZbcnStarterConfigApplication.class, args);
+    }
+
+}
---
+++ com-zbcn-utils-starter/com-zbcn-starter/src/main/java/com/zbcn/starter/ComZbcnStarterApplication.java
@@ -0,0 +1,13 @@
+package com.zbcn.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComZbcnStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComZbcnStarterApplication.class, args);
+    }
+
+}
